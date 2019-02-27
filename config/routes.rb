d271bb2e1ddf91a54330904c2e39b91a6186ac32
Henry Edwards <tardiszilla@gmail.com>
2019-02-27 15:47:28 -0500

    working skills page complete, but needs more detail/conditionals. details page begun

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,39 @@
 Rails.application.routes.draw do
+
+  #Site Navigation
   root 'sitenav#home'
+
+  #Create Method ==> Planned Character || Quick Character || Random Character
   get 'create' => 'creator#create_method'
 
+  # Planned Character - BACKGROUND - ==> Class
   get 'background' => 'creator#background'
   get 'changeback' => 'creator#change_background'
+
+  #Planned Character - CLASS - ==> Abilities
   get 'class' => 'creator#class_1'
   get 'changeclass1' => 'creator#change_class1'
+
+  #Planned Character - ABILITIES - ==> Skills
   get 'abilities' => 'creator#abilities'
-  get 'changeabilities' => 'creator#change_abilities'
-  get 'skills' => 'creator#skills'
   post 'abilities' => 'creator#skills'
   post 'skills' => 'creator#skills'
+
+  #Planned Character - SKILLS - ==> Details
+  get 'skills' => 'creator#skills'
+  post 'skills' =>'creator#details'
+  post 'details' =>'creator#details'
+
+  #Planned Character - FEATS - "Need a table for these... probably..."
+
+  #Planned Character - DETAILS ==> ??Show Character??
+  get 'details' => 'creator#details'
+  post 'details' => 'creator#show_character'
+  post 'showchar' => 'creator#show_character'
+
+  #Show Character / Character Sheet ==> Level Up!? || Edit!? || etc?
   get 'showchar' => 'creator#show_character'
+
 
   get 'quick' => 'creator#templates'
   get 'random' => 'creator#randomize'
